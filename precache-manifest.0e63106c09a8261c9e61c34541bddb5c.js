b9d9a0605b0916913cd80624ef24b87f7bf68902
Manuilenkoart <manuilenkoart@gmail.com>
2020-01-11 23:55:27 +0200

    Updates

---
+++ precache-manifest.0e63106c09a8261c9e61c34541bddb5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc619bb83a9e9e3d212a163bed874633",
+    "url": "/goit-react-hw-06-hook-phonebook/index.html"
+  },
+  {
+    "revision": "51b1797f2c1e3df8d452",
+    "url": "/goit-react-hw-06-hook-phonebook/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "4f32ffa2bb21d76897a6",
+    "url": "/goit-react-hw-06-hook-phonebook/static/css/main.37f1a7db.chunk.css"
+  },
+  {
+    "revision": "51b1797f2c1e3df8d452",
+    "url": "/goit-react-hw-06-hook-phonebook/static/js/2.e89c3817.chunk.js"
+  },
+  {
+    "revision": "4f32ffa2bb21d76897a6",
+    "url": "/goit-react-hw-06-hook-phonebook/static/js/main.557c474d.chunk.js"
+  },
+  {
+    "revision": "d767539b3597ebb99523",
+    "url": "/goit-react-hw-06-hook-phonebook/static/js/runtime-main.ffef7358.js"
+  }
+]);
